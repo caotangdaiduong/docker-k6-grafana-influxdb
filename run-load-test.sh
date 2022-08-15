8b52e8bf88a5ec717b65b3fdd41d1a74e37c9ba0
caotangdaiduong <dinhthe3993@gmail.com>
2022-08-15 20:46:59 +0700

    Update run-load-test.sh

--- run-load-test.sh
+++ run-load-test.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
+FILE=${1?Error: no file name given, ex: /scripts/ewoks.js}
+
 docker-compose up -d influxdb grafana
 get_public_ip=$(grep -m 1 -oE '^[0-9]{1,3}(\.[0-9]{1,3}){3}$' <<< "$(wget -T 10 -t 1 -4qO- "http://ip1.dynupdate.no-ip.com/" || curl -m 10 -4Ls "http://ip1.dynupdate.no-ip.com/")")
 echo "--------------------------------------------------------------------------------------"
 echo "Load testing with Grafana dashboard http://$get_public_ip:3000/d/k6/k6-load-testing-results"
 echo "--------------------------------------------------------------------------------------"
-docker-compose run --rm k6 run /scripts/ewoks.js
+docker-compose run --rm k6 run $FILE
